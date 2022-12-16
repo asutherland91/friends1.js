540069614fec239a48b39041f1e1e1b34ceb6d40
Adriane Sutherland <asutherland91@gmail.com>
2022-12-15 16:01:05 -0800

    Add two arrays

---
+++ friends.js
@@ -0,0 +1,10 @@
+var arrays = ["Brooke", "Tim", "Randy", "Jerad", "Isabelle"]
+
+console.log(arrays[2])
+console.log(arrays[4])
+console.log(arrays[0])
+
+var arrays2 = ["LA", "New York", "Seattle", "Japan", "Bali"]
+
+arrays2.push("Singapore")
+console.log(arrays2)
